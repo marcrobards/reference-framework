3bbced3099220d0efa1f9786d5653e5c867f1807
Marc Robards <marc@robards.org>
2018-01-18 14:59:49 -0500

    added font-awesome

---
+++ Reference.Framework.Web/Controllers/GearController.cs
@@ -0,0 +1,17 @@
+﻿namespace Reference.Framework.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class GearController : Controller
+    {
+        // GET: Gear
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

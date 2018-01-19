5fc9c069cdea437ece210ed9204aa7709aa53389
Marc Robards <marc@robards.org>
2018-01-19 16:21:25 -0500

    updated bootstrap, added gear page, updated layout

---
+++ Reference.Framework.Web/Controllers/WorkoutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reference.Framework.Web.Controllers
+{
+    public class WorkoutController : Controller
+    {
+        // GET: Workout
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
